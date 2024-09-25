72067108480bf09b960a71bedd77c1d35532ff6e
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-09-25 10:11:39 +0700

    feat: disable hide dotfiles in neotree

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- neotree
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+    },
+  },
+})
