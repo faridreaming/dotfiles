0ee1036abc080c1313499545941cb28ab5f97946
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-10-06 22:02:46 +0700

    feat: fresh lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- neotree
-require("neo-tree").setup({
-  filesystem = {
-    filtered_items = {
-      hide_dotfiles = false,
-    },
-  },
-})
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,3 @@
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
-
--- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
