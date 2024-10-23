92967ec81337eef3d840313854cbeb44ddbb35c5
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-10-23 20:28:14 +0700

    fish: add fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Theme
+set -g theme_title_use_abbreviated_path no
+set -g theme_color_scheme catppuccin-mocha
+
+# Greeting
+function fish_greeting
+    echo Assalamu\'alaikum, (whoami)!
+    echo The time is (set_color yellow; date "+%A, %B %d, %Y"; set_color normal)
+    echo This system is called $hostname
+end
