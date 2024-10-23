86ffbdf09d26bcc5880e7e84c415e7a426fcc6ba
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-10-23 20:58:29 +0700

    fish: change prompt style

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,12 @@ end
 # Theme
 set -g theme_title_use_abbreviated_path no
 set -g theme_color_scheme catppuccin-mocha
+set -g theme_display_user yes
+set -g theme_display_sudo_user yes
+set -g theme_display_hostname yes
+set -g theme_date_format "+%d/%m/%y %H:%M:%S"
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt "</> "
 
 # Greeting
 function fish_greeting
