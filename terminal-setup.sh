b0e9b7e15150c3be41cc752f22f4be73362bc34c
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-10-23 19:45:46 +0700

    terminal: add terminal setup script

---
+++ terminal-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Download terminal emulator and shell
+sudo pacman -S --noconfirm kitty fish
+
+# Download nerd fonts
+yay -S ttf-jetbrains-mono-nerd
+
+# Download shell fish theme
+curl -L https://get.oh-my.fish | fish 
+omf install bobthefish
+omf theme bobthefish
+
+# Change shell for user
+chsh -s /usr/bin/fish
+
+# Apply configs
+stow --adopt kitty fish
+
+echo "Some changes require a system reboot!"
