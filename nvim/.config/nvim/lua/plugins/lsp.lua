085af93cac4144eb2140bf4a7f7c880065557855
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-10-24 10:04:43 +0700

    nvim: add formatter

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -10,6 +10,27 @@ return {
         "typescript-language-server",
         "css-lsp",
         "html-lsp",
+        "prettier",
+        "marksman",
+        "lua-language-server",
+      })
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local null_ls = require("null-ls")
+
+      opts.sources = opts.sources or {}
+
+      -- Setup Prettier as a formatter for various filetypes
+      opts.sources = vim.list_extend(opts.sources, {
+        null_ls.builtins.formatting.prettier.with({
+          filetypes = { "markdown", "javascript", "typescript", "css", "html" },
+        }),
+        null_ls.builtins.formatting.stylua.with({
+          filetypes = { "lua" },
+        }),
       })
     end,
   },
