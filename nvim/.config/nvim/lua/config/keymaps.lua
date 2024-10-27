d0ea2f6c90e1df3de3b22b86ae03c5314ac08f59
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-10-28 00:32:22 +0700

    nvim: add basic keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
-
--- Copy and paste
-keymap.set({ "v", "n" }, "<C-c>", '"+y')
-keymap.set({ "v", "n" }, "<C-v>", '"+p')
+opts.desc = "Select all"
+keymap.set("n", "<C-a>", "gg<S-v>G", opts)
 
 -- Yank
-keymap.set("n", "Y", "_y$", { desc = "Inline yank" })
+opts.desc = "Inline yank"
+keymap.set("n", "Y", "_y$", opts)
+
+-- Split window
+opts.desc = "Split window below"
+keymap.set("n", "ss", ":split<Return>", opts)
+opts.desc = "Split window right"
+keymap.set("n", "sv", ":vsplit<Return>", opts)
