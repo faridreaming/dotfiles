1cda38a471576b049cb932563bdb43359f06242a
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-11-01 06:16:44 +0700

    chore(fish): disable bobthefish theme

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,10 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Theme
-set -g theme_title_use_abbreviated_path no
-set -g theme_color_scheme catppuccin-mocha
-set -g theme_date_format "+%d/%m/%y %H:%M:%S"
+# Bobthefish theme
+# set -g theme_title_use_abbreviated_path no
+# set -g theme_color_scheme user
+# set -g theme_date_format "+%d/%m/%y %H:%M:%S"
 
 # Greeting
 function fish_greeting
