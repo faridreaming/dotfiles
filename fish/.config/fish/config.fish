2da2b758f4b6faf992b14d0958e4e989b3b65f80
faridreaming <97384053+faridreaming@users.noreply.github.com>
2024-12-24 17:12:37 +0700

    feat(fish): change theme to starship

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    starship init fish | source
 end
 
-# Bobthefish theme
-# set -g theme_title_use_abbreviated_path no
-# set -g theme_color_scheme user
-# set -g theme_date_format "+%d/%m/%y %H:%M:%S"
-
-# Greeting
 function fish_greeting
     fastfetch
 end
